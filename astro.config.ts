ddd25fefd41b6a800ecf6065befb6d2fdc3953be
asninee <nine96as@proton.me>
2024-05-28 19:56:31 +0100

    Refactor astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [tailwind(), compress()],
 });
