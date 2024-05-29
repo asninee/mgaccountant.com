782df856cc43ba6ecea4f962ca5a74fba535d624
asninee <nine96as@proton.me>
2024-05-29 20:04:50 +0100

    Add shadcn, astro-icon pkgs

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,15 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-});
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    icon(),
+  ],
+})
