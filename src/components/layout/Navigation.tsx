a24fad1c8aed6de6d3d5323e6690a51d0b7e885c
asninee <nine96as@proton.me>
2024-06-19 17:28:36 +0100

    Tweak font-size for navbar

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -78,8 +78,8 @@ const ListItem = React.forwardRef<
           )}
           {...props}
         >
-          <div className='text-sm font-medium leading-none'>{title}</div>
-          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+          <div className='font-medium leading-none'>{title}</div>
+          <p className='line-clamp-2 leading-snug text-muted-foreground'>
             {children}
           </p>
         </a>
