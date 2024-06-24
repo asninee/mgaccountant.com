07b0cc733a8fab48eb25f2519332b8af6f983ebb
asninee <nine96as@proton.me>
2024-06-24 23:23:37 +0100

    Format file

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
 import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-
-import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
