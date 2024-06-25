eddf7b66ac3ac287c0da7a5324bde36fcdc2a83c
asninee <nine96as@proton.me>
2024-06-25 18:55:52 +0100

    Switch adapter to vercel, rm cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,15 @@
-import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mgaccountant.com',
-  adapter: cloudflare(),
+  adapter: vercel(),
   output: 'hybrid',
   integrations: [
     tailwind({
