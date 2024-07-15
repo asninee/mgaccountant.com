f47aa39d1f1892048d022cb38e78debf0762da1f
asninee <nine96as@proton.me>
2024-07-15 14:34:02 +0100

    Format file

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
