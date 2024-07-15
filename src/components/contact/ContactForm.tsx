ac48b522c4da30c93e7a538bc1c0e1863ff6fd47
asninee <nine96as@proton.me>
2024-07-15 15:08:46 +0100

    Revamp ContactForm cmpt; complete submit logic

---
+++ src/components/contact/ContactForm.tsx
@@ -0,0 +1,166 @@
+import { toast } from 'sonner'
+import { Button } from '../ui/button'
+import { Card, CardContent, CardFooter } from '../ui/card'
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
+import { Textarea } from '../ui/textarea'
+
+const accessKey = import.meta.env.PUBLIC_W3FORMS_KEY
+
+const ContactForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const data = new FormData(e.currentTarget)
+    e.preventDefault()
+
+    const object = Object.fromEntries(data)
+    const json = JSON.stringify(object)
+
+    const promise = fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: json,
+    })
+
+    toast.promise(promise, {
+      loading: 'Sending...',
+      success: () => {
+        return 'Email was sent successfully!'
+      },
+      error: 'Something went wrong!',
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='mt-3 flex flex-col'>
+      <input type='hidden' name='access_key' value={accessKey} />
+      <Card className='bg-neutral-100 sm:p-4'>
+        <CardContent className='pt-6'>
+          <div className='flex flex-col gap-8'>
+            <div className='flex flex-wrap gap-6'>
+              <div className='flex flex-col space-y-2 grow'>
+                <Label htmlFor='fname'>
+                  First Name <span className='text-red-500'>*</span>
+                </Label>
+                <Input
+                  className='bg-white'
+                  name='fname'
+                  required
+                  placeholder='Your first name'
+                />
+              </div>
+              <div className='flex flex-col space-y-2 grow'>
+                <Label htmlFor='lname'>
+                  Last Name <span className='text-red-500'>*</span>
+                </Label>
+                <Input
+                  className='bg-white'
+                  name='lname'
+                  required
+                  placeholder='Your last name'
+                />
+              </div>
+            </div>
+            <div className='flex flex-wrap gap-6'>
+              <div className='flex flex-col space-y-2 grow'>
+                <Label htmlFor='email'>
+                  Email Address <span className='text-red-500'>*</span>
+                </Label>
+                <Input
+                  className='bg-white'
+                  type='email'
+                  name='email'
+                  required
+                  placeholder='Your email address'
+                />
+              </div>
+              <div className='flex flex-col space-y-2 grow'>
+                <Label htmlFor='cname'>Company Name</Label>
+                <Input
+                  className='bg-white'
+                  name='cname'
+                  placeholder='Your company name'
+                />
+              </div>
+            </div>
+            <div className='flex flex-col space-y-2'>
+              <Label htmlFor='number'>Contact Number</Label>
+              <Input
+                type='tel'
+                className='bg-white'
+                name='number'
+                placeholder='Your contact number'
+              />
+            </div>
+            <div className='flex flex-col space-y-2'>
+              <Label htmlFor='message'>
+                Message <span className='text-red-500'>*</span>
+              </Label>
+              <Textarea
+                className='bg-white'
+                name='message'
+                required
+                placeholder='Type your message here'
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button type='submit' className='text-lg'>
+            Submit
+          </Button>
+          <div id='result'></div>
+        </CardFooter>
+      </Card>
+    </form>
+  )
+}
+
+export default ContactForm
+
+{
+  /* <script is:inline>
+  const form = document.getElementById('form')
+  const result = document.getElementById('result')
+
+  form.addEventListener('submit', function (e) {
+    const formData = new FormData(form)
+    e.preventDefault()
+
+    const object = Object.fromEntries(formData)
+    const json = JSON.stringify(object)
+
+    result.innerHTML = 'Please wait...'
+
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: json,
+    })
+      .then(async response => {
+        let json = await response.json()
+        if (response.status == 200) {
+          result.innerHTML = JSON.message
+        } else {
+          console.log(Response)
+          result.innerHTML = json.message
+        }
+      })
+      .catch(error => {
+        console.log(Error)
+        result.innerHTML = 'Something went wrong!'
+      })
+      .then(function () {
+        form.reset()
+        setTimeout(() => {
+          result.style.display = 'none'
+        }, 3000)
+      })
+  })
+</script> */
+}
