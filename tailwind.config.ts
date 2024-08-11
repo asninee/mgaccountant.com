1af59c8d9ddbaf9902568fe12a7e276ed2f899a7
asninee <nine96as@proton.me>
2024-08-11 16:20:38 +0100

    Add bg-img

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,9 @@ export default {
       },
     },
     extend: {
+      backgroundImage: {
+        'header-image': "url('/src/images/hero.webp')",
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
